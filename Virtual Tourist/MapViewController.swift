4790309ac48384f4e97b0298176fc62ba1d9f781
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-11-17 13:55:26 -0800

    Implement long tap gesture recognizer and add pin methods

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -6,21 +6,81 @@
 //  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
+    // MARK: Properties
+    
+    let annotation = MKPointAnnotation()
+    let annotationArray: [MKPointAnnotation] = []
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Implement the tap gesture recognizer
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.handleLongPress(_:)))
+        longPressRecognizer.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(longPressRecognizer)
+        
+        self.mapView.delegate = self
+    }
+    
+    @objc func handleLongPress(_ gestureRecognizer : UIGestureRecognizer) {
+        if gestureRecognizer.state != .began { return }
+        print("Tap gesture recognized")
+        
+        // Create the annotation
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let newCoordinate = self.mapView.convert(touchPoint, toCoordinateFrom:self.mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        
+        // Add the annotation
+        mapView.addAnnotation(annotation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
-
-
+    
+    // Show browser and navigate to media URL when annotation is tapped
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.open(URL(string: toOpen)!, options: [:]) { (success) in
+                }
+            }
+        }
+    }
+    
 }
+
+extension MapViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return !(touch.view is MKPinAnnotationView)
+    }
+}
+
 
