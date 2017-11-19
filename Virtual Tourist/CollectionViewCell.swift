d71875697455d44f38a7c04f972b067d7ee5a822
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-11-18 17:18:26 -0800

    Implement collection view cells in CollectionViewController

---
+++ Virtual Tourist/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Virtual Tourist
+//
+//  Created by Tomas Sidenfaden on 11/18/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionCellLabel: UILabel!
+}
