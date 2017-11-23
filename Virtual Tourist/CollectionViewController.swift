456239c369a6f017a5a3509e1094a48065aac15b
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-11-23 10:57:39 -0800

    Reorganize CollectionViewController class

--- Virtual Tourist/CollectionViewController.swift
+++ Virtual Tourist/CollectionViewController.swift
@@ -12,14 +12,41 @@ import MapKit
 
 class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    // MARK: Outlets
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var collectionFlow: UICollectionViewFlowLayout!
-    //var gridCollectionView: UICollectionView!
-    //var collectionViewFlowLayout: CollectionViewFlowLayout!
+    
+    // MARK: Properties
     
     var innerSpace: CGFloat = 1.0
     var numberOfCellsOnRow: CGFloat = 3.0
     var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionFlow.minimumLineSpacing = 1.0
+        collectionFlow.minimumInteritemSpacing = 1.0
+        collectionFlow.scrollDirection = .vertical
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        collectionFlow.itemSize = CGSize(width:itemWidth(), height: itemWidth())
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+    }
+    
+    func itemWidth() -> CGFloat {
+        return ((UIScreen.main.bounds.width - (self.innerSpace * 2)) / self.numberOfCellsOnRow)
+    }
     
     // MARK: - UICollectionViewDataSource protocol
     
@@ -47,28 +74,6 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        collectionFlow.minimumLineSpacing = 1.0
-        collectionFlow.minimumInteritemSpacing = 1.0
-        collectionFlow.scrollDirection = .vertical
-    }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
-        collectionFlow.itemSize = CGSize(width:itemWidth(), height: itemWidth())
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-    }
-    
-    func itemWidth() -> CGFloat {
-        return ((UIScreen.main.bounds.width - (self.innerSpace * 2)) / self.numberOfCellsOnRow)
-    }
-
 }
 
