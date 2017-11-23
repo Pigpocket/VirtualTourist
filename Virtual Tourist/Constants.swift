f0342c03a1186df95ec1ee491a31297667d8257e
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-11-23 12:28:22 -0800

    Add networking code from SleepingInTheLibrary app

---
+++ Virtual Tourist/Constants.swift
@@ -0,0 +1,51 @@
+//
+//  Constants.swift
+//  Virtual Tourist
+//
+//  Created by Tomas Sidenfaden on 11/23/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // MARK: Flickr
+    struct Flickr {
+        static let APIBaseURL = "https://api.flickr.com/services/rest/"
+    }
+    
+    // MARK: Flickr Parameter Keys
+    struct FlickrParameterKeys {
+        static let Method = "method"
+        static let APIKey = "api_key"
+        static let GalleryID = "gallery_id"
+        static let Extras = "extras"
+        static let Format = "format"
+        static let NoJSONCallback = "nojsoncallback"
+    }
+    
+    // MARK: Flickr Parameter Values
+    struct FlickrParameterValues {
+        static let APIKey = "YOUR API KEY HERE"
+        static let ResponseFormat = "json"
+        static let DisableJSONCallback = "1" /* 1 means "yes" */
+        static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
+        static let GalleryID = "5704-72157622566655097"
+        static let MediumURL = "url_m"
+    }
+    
+    // MARK: Flickr Response Keys
+    struct FlickrResponseKeys {
+        static let Status = "stat"
+        static let Photos = "photos"
+        static let Photo = "photo"
+        static let Title = "title"
+        static let MediumURL = "url_m"
+    }
+    
+    // MARK: Flickr Response Values
+    struct FlickrResponseValues {
+        static let OKStatus = "ok"
+    }
+}
---
+++ Virtual Tourist/GCDBlackBox.swift
@@ -0,0 +1,15 @@
+//
+//  GCDBlackBox.swift
+//  Virtual Tourist
+//
+//  Created by Tomas Sidenfaden on 11/23/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+
+func performUIUpdatesOnMain(_ updates: @escaping () -> Void) {
+    DispatchQueue.main.async {
+        updates()
+    }
+}
