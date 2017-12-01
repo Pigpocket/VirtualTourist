e4dd32f5117411889efd5ca60f05ef913bb44bd8
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-11-30 18:25:12 -0800

    Change tab bar to Toolbar in CollectionViewController

--- Virtual Tourist/CollectionViewController.swift
+++ Virtual Tourist/CollectionViewController.swift
@@ -14,7 +14,7 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     
     // MARK: Outlets
     
-    @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var newCollectionButton: UIBarButtonItem!
     @IBOutlet weak var MapView: MKMapView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var collectionFlow: UICollectionViewFlowLayout!
@@ -27,6 +27,13 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     var numberOfCellsOnRow: CGFloat = 3.0
     //var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
     var images = Image.imageGallery
+    
+    // MARK: Actions
+    
+    @IBAction func newCollectionAction(_ sender: Any) {
+        
+    }
+    
     
     // MARK: Lifecycle
     
