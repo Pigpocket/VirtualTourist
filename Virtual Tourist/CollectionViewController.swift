aff8aeccbebd7e0608d1f569a7581e1468b8acfc
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-12-02 21:42:06 -0800

    Eliminate unnecessary code

--- Virtual Tourist/CollectionViewController.swift
+++ Virtual Tourist/CollectionViewController.swift
@@ -21,20 +21,15 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     
     // MARK: Properties
     
-    var image: Image?
     var pin = Pin()
-    
     var innerSpace: CGFloat = 1.0
     var numberOfCellsOnRow: CGFloat = 3.0
-    //var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
-    var images = Image.imageGallery
     
     // MARK: Actions
     
     @IBAction func newCollectionAction(_ sender: Any) {
         
     }
-    
     
     // MARK: Lifecycle
     
