782abc414d405bf303e815998daaf571d68d4205
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-01-20 16:46:51 -0800

    Removed DerivedData

--- DerivedData/Virtual Tourist/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Cache.build/Objects-normal/x86_64/Cache_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/tomassidenfaden/Projects/Virtual-Tourist/DerivedData/Virtual\ Tourist/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Cache.build/DerivedSources/Cache_vers.c
