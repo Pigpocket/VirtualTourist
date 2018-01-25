e31c6dc03369900cd4a660e33bc2cee2dd1654c5
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-01-25 11:11:29 -0800

    Remove derived Data

--- DerivedData/Virtual Tourist/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Cache.build/DerivedSources/Cache_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char CacheVersionString[];
- extern const double CacheVersionNumber;
-
- const unsigned char CacheVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Cache  PROJECT:Pods-1" "\n";
- const double CacheVersionNumber __attribute__ ((used)) = (double)1.;
